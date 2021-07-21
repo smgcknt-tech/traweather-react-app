67aaed199c19a454cdd9be2da9739d69a3430939
kanta shimoguchi <business.smgcknt@gmail.com>
2021-07-21 15:47:06 +0900

    finished setting up nodemon.js
    installed nodemon

---
+++ backend/server.js
@@ -0,0 +1,9 @@
+import express from 'express';
+const app = express();
+app.get("/", (req, res) => {
+    res.send("server is ready");
+});
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
+    console.log("serve at http://localhost:${port}")
+})
