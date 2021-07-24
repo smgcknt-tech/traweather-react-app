2c0a27b40b11ca577fdc4be5edaffc9169a04b82
kanta shimoguchi <business.smgcknt@gmail.com>
2021-07-24 09:47:04 +0900

    adjusted header's text size

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,8 +4,8 @@ function App() {
   return (
     <div class="grid-container">
       <header class="row center">
-        <div>traweather</div>
-        <p>~market prediction for day traders~</p>
+        <div class="logo">traweather</div>
+        <p class="catch_copy">~market prediction for day traders~</p>
       </header>
       <main>
 
