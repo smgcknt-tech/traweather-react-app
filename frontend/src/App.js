0d72b8aa1e12f6ce83863e0efa96a72ef8521acb
kanta shimoguchi <business.smgcknt@gmail.com>
2021-07-31 17:55:48 +0900

    changed headers layout

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,18 +1,17 @@
 import "./App.scss";
-import { BrowserRouter, Route, Link } from 'react-router-dom';
+import { BrowserRouter, Route} from 'react-router-dom';
 import TopPage from "./views/TopPage";
+import Header from "./components/Header";
 
 function App() {
   return (
     <BrowserRouter>
       <div className="grid-container">
-        <header className="row center">
-          <Link className="logo" to="/">traweather</Link>
-          <p className="catch_copy">~market prediction for day traders~</p>
+        <header>
+          <Header/>
         </header>
         <main>
           <Route path="/" component={TopPage} exact/>
-
         </main>
       </div>
     </BrowserRouter>
