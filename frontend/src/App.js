a16b96c3f551c14ef47579ea2e0d0f5b1e48f429
kanta shimoguchi <business.smgcknt@gmail.com>
2021-07-31 21:57:14 +0900

    created NavBar component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,17 +1,21 @@
 import "./App.scss";
-import { BrowserRouter, Route} from 'react-router-dom';
+import { BrowserRouter, Route } from 'react-router-dom';
 import TopPage from "./views/TopPage";
 import Header from "./components/Header";
+import NavBar from "./components/NavBar";
 
 function App() {
   return (
     <BrowserRouter>
       <div className="grid-container">
         <header>
-          <Header/>
+          <Header />
         </header>
+        <nav>
+          <NavBar/>
+        </nav>
         <main>
-          <Route path="/" component={TopPage} exact/>
+          <Route path="/" component={TopPage} exact />
         </main>
       </div>
     </BrowserRouter>
---
+++ frontend/src/components/NavBar.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export default function NavBar() {
+    return (
+        <div className="nav_bar">
+            <ul　className="nav_menu">
+                <li><i class="fas fa-cloud-sun"></i></li>
+                <li><i class="fas fa-chess-pawn"></i></li>
+                <li><i class="fas fa-chart-bar"></i></li>
+                <li><i class="fas fa-book"></i></li>
+                <li><i className="fas fa-cog"></i></li>
+            </ul>
+        </div>
+    )
+}
