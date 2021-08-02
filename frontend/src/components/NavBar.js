1126607e2ff672b8244e69efa0f880041b61dd0f
kanta shimoguchi <business.smgcknt@gmail.com>
2021-08-02 11:31:48 +0900

    implemented :hover  effect for nav_bars' btn

--- frontend/src/components/NavBar.js
+++ frontend/src/components/NavBar.js
@@ -5,7 +5,7 @@ export default function NavBar() {
     return (
         <div className="nav_bar">
             <ul　className="nav_menu">
-                <Link to="#"><li><i class="fas fa-cloud-sun"></i><p>地合</p></li></Link>
+                <Link to="/"><li><i class="fas fa-cloud-sun"></i><p>地合</p></li></Link>
                 <Link to="/plan"><li><i class="fas fa-chess-pawn"></i><p>計画</p></li></Link>
                 <Link to="#"><li><i class="fas fa-chart-bar"></i><p>結果</p></li></Link>
                 <Link to="#"><li><i class="fas fa-book"></i><p>振返</p></li></Link>
