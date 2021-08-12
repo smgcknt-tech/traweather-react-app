05d2ca5dd2cf1fa234078164b2b5821de4a684b7
kanta shimoguchi <business.smgcknt@gmail.com>
2021-08-12 16:14:59 +0900

    improved search bar

--- frontend/src/components/SearchBar.js
+++ frontend/src/components/SearchBar.js
@@ -1,20 +1,43 @@
-import React from 'react'
+import React, { useState } from 'react';
 import "../styles/components/SearchBar.scss";
 
 export default function SearchBar(props) {
-    const { stockList } = props
+    const [filteredData, setFilteredData] = useState([]);
+    const [inputValue,setInputValue] = useState("");
+    const { stockList } = props;
+    const handleFilter = (event) => {
+        const searchWord = event.target.value;
+        setInputValue(searchWord);
+        const newFilter = stockList.filter((stock) => {
+            return stock.code.includes(searchWord);
+        });
+        if (searchWord === "") {
+            setFilteredData([]);
+        } else {
+            setFilteredData(newFilter);
+        }
+    }
+    const handleSelect = (event)=>{
+        const selectedStock = event.target.textContent.split(":")[1];
+        setInputValue(selectedStock);
+    }
+
+
+
     return (
         <div className="row center">
             <div className="column search">
-                <div className="row searchInputs">
-                    <input type="text" placeholder="証券番号を入力してください" />
-                    <span className="searchIcon"><i className="fas fa-search"></i></span>
+                <div className="row search_inputs">
+                    <input type="text" placeholder="証券番号を半角で入力してください" value={inputValue} onChange={handleFilter} />
+                    <span className="search_icon"> <i className="fas fa-search"></i></span>
                 </div>
-                <div className="row dataResult">
-                    {stockList.map((value, key) => {
-                        return <p className="dataItem">{value.code}:{value.stockname}</p>
-                    })}
-                </div>
+                {(filteredData.length !== 0) && (
+                    <div className="column data_result">
+                        {filteredData.slice(0, 15).map((value, key) => {
+                            return <p key={key} className="data_item" onClick={handleSelect}>{value.code} : {value.stockname}</p>;
+                        })}
+                    </div>
+                )}
             </div>
         </div>
     )
