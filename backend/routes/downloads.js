d879597b6cec54531914df5d79e568a1f5cd3ba0
kanta shimoguchi <business.smgcknt@gmail.com>
2021-10-30 14:15:26 +0900

    fix: deleted unnecessary code.

--- backend/routes/downloads.js
+++ backend/routes/downloads.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import aws from 'aws-sdk';
-import csv from 'csvtojson'
-import iconv from 'iconv-lite'
+import csv from 'csvtojson';
+import iconv from 'iconv-lite';
 import { env } from '../configs/config.js';
 import { api } from '../models/api.js';
 export const download_router = express.Router();
@@ -10,7 +10,6 @@ aws.config.update({
     accessKeyId: env.AWSAccessKeyIdForS3,
     secretAccessKey: env.AWSSecretKeyForS3,
 });
-
 const s3 = new aws.S3();
 export const dataSets = [
     ['traweather-bucket/csv', "japan-all-stock-prices-2.csv", api.upsert_latest_stock],
@@ -21,19 +20,16 @@ export const downloadCsv = async (dataSets) => {
             const file = s3
                 .getObject({ Bucket: data[0], Key: data[1], })
                 .createReadStream()
-                .pipe(iconv.decodeStream('Shift_JIS'))
+                .pipe(iconv.decodeStream('Shift_JIS'));
             const json = await csv().fromStream(file);
-            const values = json.map((obj) => Object.values(obj))
-            await data[2](values)
-            outcome.push(`${data[1]} has been upserted `)
+            const values = json.map((obj) => Object.values(obj));
+            await data[2](values);
         } catch (err) {
-            console.log(err)
+            console.log(err);
         }
     });
 }
-
-
 download_router.post('/csv', async (req, res) => {
-    await downloadCsv(dataSets)
-    res.send("csv downloaded")
+    await downloadCsv(dataSets);
+    res.send("csv downloaded");
 });
