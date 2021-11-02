7b703d22755d40814e5a90521ffc945d92731187
kanta shimoguchi <business.smgcknt@gmail.com>
2021-11-02 17:27:53 +0900

    db: changed schema

--- backend/record.sql
+++ backend/record.sql
@@ -1,1 +1,6 @@
 -- file to keep record of change on DB
+
+-- 20211102 Already implemented in the production environment.
+ALTER TABLE trade_plan ALTER COLUMN created_at SET DEFAULT CURRENT_TIMESTAMP;
+ALTER TABLE trade_result ALTER COLUMN created_at SET DEFAULT CURRENT_TIMESTAMP;
+ALTER TABLE trade_feed_back ALTER COLUMN created_at SET DEFAULT CURRENT_TIMESTAMP;
